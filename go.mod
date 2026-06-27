54d463f3d31c3d3f950ef1af2011e4c14f568f0f
newajDev <newaj.design@gmail.com>
2026-06-27 20:09:33 +0600

    feat: setup Echo server with health check and validator

--- go.mod
+++ go.mod
@@ -5,10 +5,8 @@ go 1.25.0
 require (
 	github.com/go-playground/validator/v10 v10.30.2
 	github.com/golang-jwt/jwt/v5 v5.3.1
-	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.13.3
-	golang.org/x/crypto v0.52.0
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.31.1
 )
@@ -29,8 +27,10 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.52.0 // indirect
 	golang.org/x/net v0.54.0 // indirect
 	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/sys v0.45.0 // indirect
 	golang.org/x/text v0.37.0 // indirect
+	golang.org/x/time v0.8.0 // indirect
 )
---
+++ internal/server/http.go
@@ -0,0 +1,38 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"spotsync/internal/config"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"gorm.io/gorm"
+)
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i any) error {
+	if err := cv.validator.Struct(i); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return nil
+}
+
+func Start(db *gorm.DB, cfg *config.Config) {
+	e := echo.New()
+	e.Validator = &CustomValidator{validator: validator.New()}
+	e.Use(middleware.Logger())
+
+	e.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "running")
+	})
+
+	port := fmt.Sprintf(":%s", cfg.Port)
+	if err := e.Start(port); err != nil {
+		e.Logger.Fatal(err)
+	}
+}
